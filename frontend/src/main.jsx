aef09b3ebd3f7115e24d46df4c24b2e53b27a4a5
devkotabipin42 <devkotabipin42@.com>
2026-05-05 12:32:25 +0900

    Add initial loading screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+const loader = document.getElementById("initial-loader");
+if (loader) {
+  loader.style.opacity = "0";
+  loader.style.transition = "opacity 220ms ease";
+  window.setTimeout(() => loader.remove(), 240);
+}
